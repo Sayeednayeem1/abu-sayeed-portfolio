e7ba2070a7e5daaa0d38ca80fd638d2317749110
Abu Sayeed <sayeed@pop-os.localdomain>
2023-09-19 14:43:58 +0600

    installed lottie animation

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FaLinkedinIn } from 'react-icons/fa';
+import Lottie from 'lottie-react';
+import Test from '../../assets/Images/contact.json';
 
 const Contact = () => {
     return (
         <section className="text-gray-600 body-font">
-            <div className="container px-5 py-24 mx-auto flex flex-wrap">
+            <div className="container mx-auto max-w-screen-xl px-5 py-24 flex flex-wrap">
                 <div className="flex flex-wrap -mx-4 mt-auto mb-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
                     <div className="w-full sm:p-4 px-4 mb-6">
                         <h1 className="title-font font-bold text-2xl mb-2 text-gray-800">Contact Me</h1>
@@ -13,25 +16,21 @@ const Contact = () => {
                         </div>
                     </div>
 
-                    <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
-                        <h2 className="title-font font-medium text-3xl text-gray-900">2.7K</h2>
-                        <p className="leading-relaxed">Users</p>
+                    <div className="p-4 sm:w-1/2 lg:w-1/6 w-1/2">
+                        <p className='bg-[#E0EDEF] hover:bg-gray-200 shadow-xl flex items-center p-6 rounded-lg cursor-pointer icon-animation'><FaLinkedinIn className='text-2xl'></FaLinkedinIn></p>
                     </div>
-                    <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
-                        <h2 className="title-font font-medium text-3xl text-gray-900">1.8K</h2>
-                        <p className="leading-relaxed">Subscribes</p>
+                    <div className="p-4 sm:w-1/2 lg:w-1/6 w-1/2">
+                        <p className='bg-[#E0EDEF] hover:bg-gray-200 shadow-xl flex items-center p-6 rounded-lg cursor-pointer icon-animation'><FaLinkedinIn className='text-2xl'></FaLinkedinIn></p>
                     </div>
-                    <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
-                        <h2 className="title-font font-medium text-3xl text-gray-900">35</h2>
-                        <p className="leading-relaxed">Downloads</p>
+                    <div className="p-4 sm:w-1/2 lg:w-1/6 w-1/2">
+                        <p className='bg-[#E0EDEF] hover:bg-gray-200 shadow-xl flex items-center p-6 rounded-lg cursor-pointer icon-animation'><FaLinkedinIn className='text-2xl'></FaLinkedinIn></p>
                     </div>
-                    <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
-                        <h2 className="title-font font-medium text-3xl text-gray-900">4</h2>
-                        <p className="leading-relaxed">Products</p>
+                    <div className="p-4 sm:w-1/2 lg:w-1/6 w-1/2">
+                        <p className='bg-[#E0EDEF] hover:bg-gray-200 shadow-xl flex items-center p-6 rounded-lg cursor-pointer icon-animation'><FaLinkedinIn className='text-2xl'></FaLinkedinIn></p>
                     </div>
                 </div>
                 <div className="lg:w-1/2 sm:w-1/3 w-full rounded-lg overflow-hidden mt-6 sm:mt-0">
-                    <img className="object-cover object-center w-full h-full" src="https://dummyimage.com/600x300" alt="stats" />
+                    <Lottie className="object-cover object-center w-full h-full" animationData={Test}></Lottie>
                 </div>
             </div>
         </section>
